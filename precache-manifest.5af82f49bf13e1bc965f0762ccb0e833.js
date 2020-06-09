e1442d914e00fecdd8671349624cc831f3cdf5de
tknbr <arnaubago1993@gmail.com>
2020-06-09 19:45:44 +0200

    Updates

---
+++ precache-manifest.5af82f49bf13e1bc965f0762ccb0e833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ee465b59d117d2f49d93624cbb561e",
+    "url": "/weather_simulation/index.html"
+  },
+  {
+    "revision": "f6abf596fb0c435ff400",
+    "url": "/weather_simulation/static/css/main.29084d4f.chunk.css"
+  },
+  {
+    "revision": "6a958144b6eabfefd133",
+    "url": "/weather_simulation/static/js/2.ad00debf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_simulation/static/js/2.ad00debf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6abf596fb0c435ff400",
+    "url": "/weather_simulation/static/js/main.aafae9b1.chunk.js"
+  },
+  {
+    "revision": "54e4479ddea17c7800b5",
+    "url": "/weather_simulation/static/js/runtime-main.d7f96897.js"
+  }
+]);
